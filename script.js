99ab3c73bab843534c9a17aca25833a26be79416
Ricardo Rodiani <ricardo@LAPTOP-ELJKMNCO.localdomain>
2021-03-22 19:09:34 -0300

    Leitura de arquvo finalizada

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-let fs = require("fs");
+const fs = require("fs");
 
-fs.readFile("entrada.txt", "utf-8", (erro, dado) => {
-    if (erro) {
-        console.log("Ocorreu algum erro ao tentar ler o arquivo -> " + erro);
-    }
-    console.log(dado);
-});
+try {
+    // read contents of the file
+    const data = fs.readFileSync("entrada.txt", "UTF-8");
+
+    // splita o conteudo de cada linha transformando num objeto
+    const lines = data.split(/\r?\n/);
+    // const array = [];
+    // lines.forEach((line) => {
+    //     const i = line.split(" ");
+    //     array.push(i);
+    // });
+    // console.log(array);
+    // let iniciasFinais = array[0];
+    // console.log(iniciasFinais);
+    let primeiraLinha = lines.shift().split(";");
+    let palavra = lines.pop().split(":").pop();
+    console.log(primeiraLinha, palavra, lines);
+} catch (err) {
+    console.error(err);
+}
+
+function Estado(nome, tipo) {
+    this.nome = nome;
+    this.tipo = tipo;
+}
