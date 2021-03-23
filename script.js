750ac179aec9a7df546db6e4e49da87be18ca4a4
Ricardo Rodiani <ricardo@LAPTOP-ELJKMNCO.localdomain>
2021-03-22 22:54:40 -0300

    filtro alfabeto adicionado

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ try {
     let iniciais = iniciaisFinais[0].trim().split(" ");
     let finais = iniciaisFinais[1].trim().split(" ");
     let palavra = lines.pop().trim().split(":").pop();
+    let alfabeto = lines
+        .map((linha) => linha.split(" "))
+        .map((elemento) =>
+            elemento.filter((elemento, indice, proprio) => {
+                return indice == 1;
+            })
+        );
+    alfabeto = [].concat.apply([], alfabeto);
+    alfabeto = alfabeto.filter(dadosUnicos);
     let outros = lines
         .map((linha) => linha.split(" "))
         .map((elemento) =>
@@ -23,15 +32,24 @@ try {
         }
     });
     let todos = [].concat(iniciais, outros, finais);
+    console.log(alfabeto);
     console.log(outros);
     console.log(iniciais);
     console.log(finais);
     console.log(todos);
+    const afd = new Automato(todos);
 } catch (err) {
     console.error(err);
 }
 
-function Automato(todos) {}
+function Automato(iniciais, outros, finais) {
+    this.estadosIniciais = iniciais;
+    this.estadosFinais = finais;
+    this.estadosOutros = outros;
+    this.gerarDot = function () {
+        let str = "";
+    };
+}
 
 function dadosUnicos(elemento, indice, self) {
     return self.indexOf(elemento) === indice;
